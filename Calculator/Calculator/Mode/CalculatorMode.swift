8adde9f0a15ef05557875ea2d8ae9a9b30d61cb2
wonhee <wonhee010@gmail.com>
2020-12-18 17:18:30 +0900

    feat: add calculatorMode enum

---
+++ Calculator/Calculator/Mode/CalculatorMode.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorMode.swift
+//  Calculator
+//
+//  Created by Wonhee on 2020/12/18.
+//
+
+import Foundation
+
+enum CalculatorMode {
+    case decimal
+    case binary
+}
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,6 +8,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private var calculatorMode: CalculatorMode = .decimal {
+        didSet {
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
